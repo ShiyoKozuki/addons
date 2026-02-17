1707941353c9736bc0f98685fb174fa03caeaf3b
Shiyo <strikinginsanity@hotmail.com>
2026-02-17 15:11:10 -0600

    Add miu's logincmnd files

---
+++ logincmd/profiles/Sayaki.lua
@@ -0,0 +1,16 @@
+require('common');
+-- Load or unload addons / plugins
+
+-- Delayed commands
+local function delayed_setup()
+	print('-- running delayed setup commands.. --');
+	-- Load or unload addons / plugins
+	AshitaCore:GetChatManager():QueueCommand(-1, '/load shorthand');
+	AshitaCore:GetChatManager():QueueCommand(-1, '/addon load inventorycounter');
+	AshitaCore:GetChatManager():QueueCommand(-1, '/sl others on');
+	-- Chatmode party has to be delayed or it won't work
+	AshitaCore:GetChatManager():QueueCommand(-1, '/chatmode party');
+	print('-- done --');
+end
+
+delayed_setup:once(10);
