01721c74690f4d6e7370ddfd4b2d7eb1940ec47c
Shiyo <strikinginsanity@hotmail.com>
2026-09-11 16:11:40 -0500

    Add print

--- libs/shiyolibs.lua
+++ libs/shiyolibs.lua
@@ -3166,7 +3166,7 @@ local function AllTalk()
                 coroutine.sleep(1);
             end
             first = false;
-            -- print(string.format(myName .. " sending alltalk command to " .. mule))
+            print(string.format(myName .. " sending alltalk command to " .. mule))
             AshitaCore:GetChatManager():QueueCommand(-1, string.format("/mst %s /global talk %u", mule, target));
         end
     end
